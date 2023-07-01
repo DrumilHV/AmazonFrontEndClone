640a9af1251af224ba26cede76f4d2aea465a2d2
drumil ved <drumilved@drumils-MacBook-Pro.local>
2023-07-01 20:50:41 +0530

    removed all warnings

--- src/Booklist.jsx
+++ src/Booklist.jsx
@@ -3,22 +3,20 @@ import Books from "./Books";
 import Title from "./Title";
 import { bookCatlog } from "./bookCatlog";
 export const Booklist = () => {
-  const getBook = (id) => {
-    bookCatlog.forEach((buk) => {
-      if (buk.id === id) {
-        console.log(buk);
-      }
-    });
-  };
+  //   const getBook = (id) => {
+  //     bookCatlog.forEach((buk) => {
+  //       if (buk.id === id) {
+  //         console.log(buk);
+  //       }
+  //     });
+  //   };
   return (
     <>
       {/* <EventExample /> */}
       <Title />
       <section className="booklist">
         {bookCatlog.map((book, index) => {
-          return (
-            <Books {...book} key={book.id} getBook={getBook} index={index} />
-          );
+          return <Books {...book} key={book.id} index={index} />;
         })}
       </section>
     </>
--- src/Books.jsx
+++ src/Books.jsx
@@ -2,30 +2,30 @@ import React from "react";
 // import { ReactDOM } from "react-dom";
 // import bookCatlog from "./bookCatlog";
 // import Booklist from "./Booklist";
-export const EventExample = () => {
-  const handleFormInput = (e) => {
-    console.log("Change happen!!");
-    console.log(e.target.name);
-    console.log(e.target.value);
-  };
-  return (
-    <section>
-      <h2 style={{ marginLeft: "1rem" }}>Some kind of Form </h2>
-      <form action="">
-        <input
-          type="text"
-          name="example"
-          onChange={handleFormInput}
-          style={{ margin: "1rem", fontSize: "2rem", color: "red" }}
-        />
-      </form>
-      <button style={{ marginLeft: "1rem" }}>click Me</button>
-    </section>
-  );
-};
+// export const EventExample = () => {
+//   const handleFormInput = (e) => {
+//     console.log("Change happen!!");
+//     console.log(e.target.name);
+//     console.log(e.target.value);
+//   };
+//   return (
+//     <section>
+//       <h2 style={{ marginLeft: "1rem" }}>Some kind of Form </h2>
+//       <form action="">
+//         <input
+//           type="text"
+//           name="example"
+//           onChange={handleFormInput}
+//           style={{ margin: "1rem", fontSize: "2rem", color: "red" }}
+//         />
+//       </form>
+//       <button style={{ marginLeft: "1rem" }}>click Me</button>
+//     </section>
+//   );
+// };
 
 const Books = (props) => {
-  const { title, auther, image, getBook, index, id } = props;
+  const { title, auther, image, index, id } = props;
   // const displayTitle = () => {
   //   console.log(title);
   // };
@@ -35,7 +35,7 @@ const Books = (props) => {
   return (
     <div>
       <article className="book">
-        <img src={image} alt={title} />
+        <img src={image} alt={title} id={id} />
         <h1>{title}</h1>
         {/* <button onClick={() => getBook(id)}>click Me</button> */}
         <h4>{auther}</h4>
