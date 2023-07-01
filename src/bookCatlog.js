285b27ecc983e5f636f4f2a7d1dbd482a8c46599
drumil ved <drumilved@drumils-MacBook-Pro.local>
2023-07-01 20:26:00 +0530

    Amazon Front end react best seller clone

--- src/bookCatlog.js
+++ src/bookCatlog.js
@@ -1,38 +1,38 @@
 import img1 from "./images/LinuxBook.jpg";
+import img2 from "./images/10x Rule.jpg";
+import img3 from "./images/Energise your mind.jpg";
+import img4 from "./images/Power of subconcius mind.jpg";
 export const bookCatlog = [
   {
     title: "10X RULE",
     auther: "Grand Cardon",
-    image:
-      "https://m.media-amazon.com/images/I/4107CbBYPLL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+    image: img2,
     id: 1,
   },
   {
-    title: "Wise and OtherWise",
-    auther: "Sudha Murti",
-    image:
-      "https://m.media-amazon.com/images/I/415n3x+l8PL._SY344_BO1,204,203,200_.jpg",
+    title: "Karma",
+    auther: "SadGuru",
+    image: img3,
     id: 2,
   },
   {
     title: "Power of Subconcius Mind",
     auther: "Josep Murphy",
-    image:
-      "https://m.media-amazon.com/images/I/51DlDekQ5-L._AC_UY436_FMwebp_QL65_.jpg",
+    image: img4,
     id: 3,
   },
   {
-    title: "A Random BOok ",
-    auther: "Another random auther",
-    image:
-      "https://m.media-amazon.com/images/I/51DlDekQ5-L._AC_UY436_FMwebp_QL65_.jpg",
-    id: 4,
-  },
-  {
-    title: "A Generic Linux Book",
-    auther: "A Gneric computer Geek",
+    title: "Linux",
+    auther: "Murty",
     image: img1,
     id: 5,
+  },
+  {
+    title:
+      "Energize Your Mind: Learn the Art of Mastering Your Thoughts, Feelings and Emotions",
+    auther: "Gaur Gopal Das",
+    image: img1,
+    id: 6,
   },
 ];
 // export default bookCatlog;
